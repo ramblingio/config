3cb56aff9fa4c0acaa142ef060f3924ff3253d96
chrkolst <christopher.kolstad@finn.no>
2016-07-05 18:52:13 +0200

    Tweak eureka config

--- src/main/java/io/rambling/ConfigserverApplication.java
+++ src/main/java/io/rambling/ConfigserverApplication.java
@@ -2,8 +2,10 @@ package io.rambling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
